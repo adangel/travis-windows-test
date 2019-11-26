286aadd2c483d9feac77f3d3076f5686198f8ab9
Andreas Dangel <andreas.dangel@adangel.org>
2019-11-26 23:08:57 +0100

    gpgconf --kill gpg-agent

--- build-deploy.sh
+++ build-deploy.sh
@@ -4,12 +4,16 @@ set -e
 ps -W | sort | tee ps-before.txt
 
 mkdir -p "$HOME/.gpg"
-gpg --no-autostart --batch --import gpg-key-pub-sec.asc
+gpg --batch --import gpg-key-pub-sec.asc
 
 
 MVN_BUILD_FLAGS="-B -V"
 ./mvnw verify $MVN_BUILD_FLAGS
 
 ps -W | sort | tee ps-after.txt
-
 diff -ub ps-before.txt ps-after.txt
+
+gpgconf --kill gpg-agent
+
+ps -W | sort | tee ps-after2.txt
+diff -ub ps-before.txt ps-after2.txt
