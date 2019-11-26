00595fa39d3ed2d0d2be96c9e14890d30310ffc6
Andreas Dangel <andreas.dangel@adangel.org>
2019-11-26 22:54:47 +0100

    add process list output

--- build-deploy.sh
+++ build-deploy.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 set -e
 
+ps -W | sort | tee ps-before.txt
+
 mkdir -p "$HOME/.gpg"
 gpg --batch --import gpg-key-pub-sec.asc
 
 
 MVN_BUILD_FLAGS="-B -V"
 ./mvnw verify $MVN_BUILD_FLAGS
+
+ps -W | sort | tee ps-after.txt
+
+diff -ub ps-before.txt ps-after.txt
